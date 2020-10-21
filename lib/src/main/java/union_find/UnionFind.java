1ba70ab8e6fb29bb759c61cf32d5a631a75384dc
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2020-10-21 20:28:14 +0300

    Add rank

--- lib/src/main/java/union_find/UnionFind.java
+++ lib/src/main/java/union_find/UnionFind.java
@@ -5,8 +5,11 @@ public class UnionFind {
   public UnionFind(int n) {
     size = n;
     parent = new int[size];
-    for (int i = 0; i < size; ++i)
+    rank = new int[size];
+    for (int i = 0; i < size; ++i) {
       parent[i] = i;
+      rank[i] = 0;
+    }
   }
 
   public int find(int u) {
@@ -20,9 +23,18 @@ public class UnionFind {
   public void union(int u, int v) {
     int ru = find(u);
     int rv = find(v);
-    if (ru != rv) parent[ru] = rv;
+    if (ru != rv) {
+      if (rank[ru] < rank[rv]) {
+        parent[ru] = rv;
+        ++rank[ru];
+      } else {
+        parent[rv] = ru;
+        ++rank[rv];
+      }
+    }
   }
 
   private int size;
   private int[] parent;
+  private int[] rank;
 }
